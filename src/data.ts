dca6bfc4de79bcce7b95642a975d0a4dcaf6d60d
Jude Nelson <judecn@gmail.com>
2019-08-01 18:55:36 -0400

    guess that the profile file name is "profile.json" if the blockstack ID given has zero-length (is falsey)

--- src/data.ts
+++ src/data.ts
@@ -160,7 +160,7 @@ function gaiaFindProfileName(network: CLINetworkAdapter,
                              hubConfig: GaiaHubConfig,
                              blockstackID?: string
 ): Promise<string> {
-  if (blockstackID === null || blockstackID === undefined) {
+  if (!blockstackID || blockstackID === null || blockstackID === undefined) {
     return Promise.resolve().then(() => 'profile.json')
   }
   else {
